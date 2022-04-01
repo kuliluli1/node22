d65ac2d170522b59bdbf404808a6a3ffdd65c630
unknown <Student@108-07.vssdomena.scv.si>
2022-04-01 12:55:40 +0200

    user modul, controller, service

---
+++ src/user/user.controller.ts
@@ -0,0 +1,19 @@
+import {Controller, Get} from '@nestjs/common';
+import {UserService} from "./user.service";
+
+@Controller('users')
+export class UserController {
+
+    constructor( private userService: UserService) {
+
+    }
+
+
+
+    @Get()
+    all() {
+        return this.userService.all();
+    }
+}
+
+
